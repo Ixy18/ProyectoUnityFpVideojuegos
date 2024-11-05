707a4b9b3756b5a889b078cade61f55ea24b9e0a
Isaac <Ixy18Gamer@gmail.com>
2024-11-05 13:55:34 +0100

    Test 1 modificado

--- Assets/Scripts/Test 1.cs
+++ Assets/Scripts/Test 1.cs
@@ -5,7 +5,7 @@ public class Test1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hola Mundo!!");
+        Debug.Log("Hola Mundo!!, estoy modificado");
     }
 
     // Update is called once per frame
